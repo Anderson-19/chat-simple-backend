4ca5fd6a56268a7d0c3a8494ce16ee9709719289
Anderson G. Gutierrez <63797161+Anderson-19@users.noreply.github.com>
2025-03-13 22:23:32 -0400

    Add files via upload

---
+++ src/sockets/sockets.gateway.ts
@@ -0,0 +1,52 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  OnGatewayConnection,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { SocketsService } from './sockets.service';
+import { CreateSocketDto } from './dto/create-socket.dto';
+import { Server, Socket } from 'socket.io';
+//import { UpdateSocketDto } from './dto/update-socket.dto';
+
+@WebSocketGateway({
+  cors: {
+    origin: [
+      'https://hoppscotch.io',
+      'http://localhost:4200',
+      'http://localhost:3001',
+    ],
+  },
+})
+export class SocketsGateway implements OnGatewayConnection {
+
+  @WebSocketServer()
+  server: Server;
+
+  constructor(private readonly socketsService: SocketsService) {}
+
+
+  @SubscribeMessage('createSocket')
+  create(@MessageBody() createSocketDto: CreateSocketDto) {
+    console.log('Create');
+    return this.socketsService.create(createSocketDto);
+  }
+
+  handleConnection() {
+
+    this.server.on('connection', (socket: Socket) => {
+      console.log('A client connected');
+    
+      socket.on('chat message', (msg) => {
+        console.log('Message received:', msg);
+        this.server.emit('chat message', msg); // Broadcast the message to all connected clients
+      });
+    
+      socket.on('disconnect', () => {
+        console.log('A client disconnected');
+      });
+    });
+  }
+
+}
